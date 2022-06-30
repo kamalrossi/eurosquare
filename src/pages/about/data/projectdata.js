158935b1ddf403a3ea48051eb8e5eea30ce5794b
kamalrossi <rossikamal@gmail.com>
2022-06-30 20:17:25 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,48 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "EuroSquare";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "EuroSquare is a food delivery app with  landing page and Dashboard having  modules namely menu, customer,order, review, reporting, inventory."
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag2: "ChartJS",
+    tag3: "D3",
+    tag4: "E-Commerce"
+  }
+];
+const projecttime = [
+  {
+    title: "UX/UI  Prototyping",
+    from: "Mar 22",
+    to: "Jun 22"
+  },
+  {
+    title: "Responsive Web Coding",
+    from: "Mar 22",
+    to: "Jun 22"
+  }
+];
+
+const projectrole = [
+  {
+    title: "Landing Page Development",
+    desc:"Prototyping and implementation of responsive landing page"
+  },
+  {
+    title: "Responsive E-Commerce Dashboard Development",
+    desc:
+      "  Responsive Admin Dashboard development with all e-commerce modules",
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
